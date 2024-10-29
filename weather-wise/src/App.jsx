3bc663fd57cef4bd3201060ba876a8351636cec7
sunny.park <seolseee@gmail.com>
2024-10-29 12:54:46 +0900

    라우터 추가

--- weather-wise/src/App.jsx
+++ weather-wise/src/App.jsx
@@ -5,6 +5,10 @@ import Home from "./pages/Home";
 import Missions from "./pages/Missions";
 import Notfound from "./pages/Notfound";
 import MissionDetails from "./pages/MissionDetails";
+import CommunityPage from './pages/CommunityPage';
+import CreatePostPage from './pages/CreatePostPage';
+import MyProfilePage from './pages/MyProfilePage';
+import MyPostPage from './pages/MyPostPage';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +19,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/missions/:id" element={<MissionDetails />} />
+        <Route path="/community" element={<CommunityPage />} />
+        <Route path="/create-post" element={<CreatePostPage />} />
+        <Route path="/myprofile" element={<MyProfilePage />} />
+        <Route path="/myposts" element={<MyPostPage />} />
         {/* 상세 페이지 경로 */}
         <Route path="*" element={<Notfound />} />
       </Routes>
