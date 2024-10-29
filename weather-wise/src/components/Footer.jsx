9a29518309c222bb8cea2612857f1be46217a05b
sunny.park <seolseee@gmail.com>
2024-10-29 12:54:14 +0900

    Footer 수정

--- weather-wise/src/components/Footer.jsx
+++ weather-wise/src/components/Footer.jsx
@@ -1,20 +1,26 @@
+import { Link } from 'react-router-dom';
 import home from "../assets/home.png";
 import community from "../assets/community.png";
 import challenge from "../assets/challenge.png";
 import rank from "../assets/rank.png";
 import "../components/Footer.css";
+
 const Footer = () => {
   return (
     <footer className="footer">
+      <Link to="/">
       <button className="footer-icon">
         <img src={home} alt="home" />
       </button>
+      </Link>
       <button className="footer-icon">
         <img src={challenge} alt="challenge" />
       </button>
+      <Link to="/community">
       <button className="footer-icon">
         <img src={community} alt="community" />
       </button>
+      </Link>
       <button className="footer-icon">
         <img src={rank} alt="rank" />
       </button>
