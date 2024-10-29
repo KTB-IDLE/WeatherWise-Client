7dbe2589b432f429b9d7708c15b895c7a04920c4
sunny.park <seolseee@gmail.com>
2024-10-29 15:53:27 +0900

    API 전송 방식 변경

--- weather-wise/src/pages/MyPostPage.jsx
+++ weather-wise/src/pages/MyPostPage.jsx
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import UserPostItem from '../components/community/UserPostItem';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './MyPostPage.css';
 // 시간 변환 유틸리티 함수 import
 import { convertArrayToDate } from '../utils/timeUtils';
+import AxiosInstance from '../utils/AxiosInstance';
 
 const MyPostPage = () => {
-    const userId = 1;
-
     const [posts, setPosts] = useState([]);
-    const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-    const accessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
 
     useEffect(() => {
         const fetchUserPosts = async () => {
+            
         try {
-            const response = await axios.get(`${apiBaseUrl}/api/boards/user/${userId}`, {
-            headers: { Authorization: `Bearer ${accessToken}` },
-            });
+            const url = `/boards/user`;
+            const response = await AxiosInstance.get(url);
 
             // 게시글을 createdAt 필드 기준으로 최신순(내림차순) 정렬
             const sortedPosts = response.data.boards.sort((a, b) => {
@@ -29,14 +25,12 @@ const MyPostPage = () => {
           });
   
             setPosts(sortedPosts); // 정렬된 게시글을 상태에 저장
-
-            setPosts(sortedPosts);
         } catch (error) {
             console.error('게시글을 불러오는 중 오류 발생:', error);
         }
         };
         fetchUserPosts();
-    }, [userId]);
+    }, []);
 
     return (
         <div className="my-post-page">
